736d137bb1b016f0b10446b0628ded49b9784c66
JonthueM <connect@jonthuemichel.com>
2020-06-13 20:39:28 -0400

    Step 55 Assignment and other assignments

---
+++ step55-AccountProject-Greets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace step55_AccountProject_Greets
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
+        }
+    }
+}
