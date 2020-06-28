e1ee749520638527c2a239513572e5a109504198
JonthueM <connect@jonthuemichel.com>
2020-06-28 13:39:41 -0400

    Assignment 152 - Catch It
    Using Catch to catch potiential errors and translating them to something... nice

---
+++ step152-CatchIt/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step152_CatchIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> divideThis = new List<int>() { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+            try
+            {
+                Console.WriteLine("Help me divide a list of numbers by giving me a number to divide with below!");
+                int divideWith = Convert.ToInt32(Console.ReadLine());
+                for (int everything = 0; everything < divideThis.Count; everything++)
+                {
+                    int sum = divideThis[everything] / divideWith;
+                    Console.WriteLine($"{divideThis[everything]} / {divideWith} = {sum}");
+                }
+
+
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please don't divide by zero");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
