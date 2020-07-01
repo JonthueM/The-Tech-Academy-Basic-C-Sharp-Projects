3068a7ed17eb4bfa7de6ee50c2738e77ff413001
JonthueM <connect@jonthuemichel.com>
2020-06-30 22:22:10 -0400

    step155-3Ways
    Three ways is using 3 different input under the same name. Hence the name you will see in code threeToOne

---
+++ step155-3Ways/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step155_3Ways
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There are many ways to reach the peak you seek!\n\n");
+            Console.WriteLine("Chose a Number!");
+            int one = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Chose a Number with a decimal");
+            decimal two = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Chose a Number that you have not used before without a decimal");
+            string three = Console.ReadLine();
+
+            //Prcoessing
+            int takeThis = 0;
+            intOperations manytoOne = new intOperations();
+            int returnValue1 = intOperations.manyToOne(one);
+            decimal returnValue2 = intOperations.manyToOne(two);
+            string pushValue = intOperations.manyToOne(three,out takeThis);
+            Console.WriteLine($"\nPath One: Your Number {one} x 10 = {returnValue1}. \nPath Two: Your Decimal Value {two} + 10050 = {returnValue2}. \n Path Three: Your number {three} / 5 = {takeThis}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ step155-3Ways/intOperations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step155_3Ways
+{
+    class intOperations
+    {
+        public static int manyToOne(int firstNumber) 
+        {
+            return firstNumber * 10;
+        }
+        public static decimal manyToOne(decimal secondNumber) 
+        {
+            return secondNumber + 10050;
+        }
+        public static string manyToOne(string thirdNumber, out int output) 
+        {
+            int converted = Convert.ToInt32(thirdNumber);
+            int takeThis = converted / 5;
+            output = takeThis;
+            return thirdNumber;
+
+        }
+    }
+}
