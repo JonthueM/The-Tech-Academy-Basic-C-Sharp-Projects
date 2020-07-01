66b20a740171f851b10920f64d425456c4f19eb2
JonthueM <connect@jonthuemichel.com>
2020-07-01 09:09:13 -0400

    step166 - The Void
    Learning to use void on method

---
+++ step166-TheVoid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step166_TheVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loading the Class
+            theVoid blackHole = new theVoid();
+
+            //Prompt & Store
+            Console.WriteLine("Random Number Please!");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Another One!");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            //Send, Receive & Print
+            Console.WriteLine($"You typed {numberOne} x {numberTwo} = ");
+            theVoid.voidOp(numberOne, numberTwo);
+
+
+
+            //Keeps it Up
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ step166-TheVoid/theVoid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step166_TheVoid
+{
+    class theVoid
+    {
+         public static void voidOp(int numOne, int numTwo) 
+        { int sum = numOne * numTwo; Console.WriteLine(sum); }
+    }
+}
