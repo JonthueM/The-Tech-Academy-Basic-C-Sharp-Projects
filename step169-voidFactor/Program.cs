6044bf35a3688bcb992b1d97b430eff0cc5495ea
JonthueM <connect@jonthuemichel.com>
2020-07-03 14:48:27 -0400

    Assignment 169
    YAY!

---
+++ step169-voidFactor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step169_voidFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load the Class
+            voidClass voidFactor = new voidClass();
+            voidClass void2 = new voidClass();
+
+
+            //Prompt & Store
+            Console.WriteLine("Type a number that you want to be divided by 2");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //Processing & Output
+            int output = 0;
+            voidClass.voidOp(number, out output);
+            Console.WriteLine($"Your {number} / 2 =");
+            voidFactor.voidOp(number);
+            Console.WriteLine($"{number} / 2 =  {output}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ step169-voidFactor/voidClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step169_voidFactor
+{
+    class voidClass
+    {
+         public void voidOp(int numOne) { int sum = numOne / 2; Console.WriteLine(sum); }
+        public static void voidOp(int numOne, out int output) { int sum = numOne / 2; output = sum; }
+
+
+    }
+}
