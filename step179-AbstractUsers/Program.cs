c5e2d717626c7de5184b22bdd2bede94d6d12e0e
JonthueM <connect@jonthuemichel.com>
2020-07-04 09:09:39 -0400

    step179-AbstractUser

---
+++ step179-AbstractUsers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step179_AbstractUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee hourly = new Employee() {firstName ="Sample", lastName="Student" };
+            hourly.SayName();
+
+
+        }
+    }
+}
