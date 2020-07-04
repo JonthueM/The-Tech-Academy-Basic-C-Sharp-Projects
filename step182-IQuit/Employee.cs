e0dae36e5abbc99947d97efdf787d69e65c90222
JonthueM <connect@jonthuemichel.com>
2020-07-04 13:16:03 -0400

    Assignment  182 - iQuit

---
+++ step182-IQuit/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step182_IQuit
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            string fullName = $"{firstName} {lastName} ";
+            Console.WriteLine(fullName);
+        }
+        public void Quit()
+        {
+            string iQuit = "has Quit";
+            Console.WriteLine(iQuit);
+        }
+    }
+}
---
+++ step182-IQuit/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step182_IQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee iQuite = new Employee() {firstName = "Jonthue", lastName = "Michel" };
+             iQuite.SayName(); iQuite.Quit();
+            Console.ReadLine();
+
+        }
+    }
+}
