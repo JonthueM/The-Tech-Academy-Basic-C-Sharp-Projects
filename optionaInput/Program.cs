b748177a4c74a5b6b3bdd0190583d31f4c3011e9
JonthueM <connect@jonthuemichel.com>
2020-07-06 11:08:18 -0400

    Fix 165 & Upload 187
    Let Get It!
    Added something to make it easier to add and do not add values to a optional para

--- optionaInput/Program.cs
+++ optionaInput/Program.cs
@@ -15,14 +15,27 @@ namespace optionaInput
 
             //Prompt & Store
             Console.WriteLine("Enter A Random Number!");
-            int randomNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Just Press Enter!");
-            Console.ReadLine();
-            //int optionalValue = Convert.ToInt32(Console.ReadLine());
+            int ranNumOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Would You Like To Enter A Second Number? Currently it is {ranNumOne} X 10.  Please Type Y or N Below!");
+            string fate = Console.ReadLine();
+            int ranNumTwo;
+            
+            if (fate == "Y")
+            {
+                Console.WriteLine("Please Enter A Second Random Number to replace 10 Below!");
+                ranNumTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"You typed {ranNumOne} to be multplied by {ranNumTwo} and your results are:");
+                int results = optionalOperation.optionalOps(ranNumOne, ranNumTwo);
+                Console.WriteLine(results);
+            }
+            else 
+            {
+                Console.WriteLine("Okie Dokie! The Results Are:");
+                int results = optionalOperation.optionalOps(ranNumOne);
+                Console.WriteLine(results);
 
-            //Passing, Receiving & Printing
-            int results = optionalOperation.optionalOps(randomNumber);
-            Console.WriteLine(results);
+            }
+                    
 
             Console.ReadLine();
 
---
+++ step187-OperatorOverload/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step187_OperatorOverload
+{
+    public class Employee : Person
+
+    {
+        public int ID { get; set; }
+        public override void SayName()
+        {
+            string fullName = $"{firstName} {lastName} ";
+            Console.WriteLine(fullName);
+        }
+        public static bool operator == (Employee employeeOne, Employee employeeTwo)
+        {
+            bool status = false;
+            if (employeeOne.ID == employeeTwo.ID)
+            { status = true; }
+            
+            return status;
+             
+        }
+        public static bool operator != (Employee employeeOne, Employee employeeTwo)
+        {
+            bool status = false;
+            if(employeeOne.ID != employeeTwo.ID) 
+            { status = false; }
+            return status;
+        }
+        
+
+
+    }
+}
