40bb6c887db1ad71ceb14683fc57382f16a1e41d
JonthueM <connect@jonthuemichel.com>
2020-07-08 22:03:12 -0400

    step196-EnumAnndddd

---
+++ step195-EnumAnnndddd/Program.cs
@@ -0,0 +1,44 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step195_EnumAnnndddd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            days days = new days();
+            DaysOfTheWeek daysOfTheWeek = DaysOfTheWeek.Wedensday;
+
+            String currentDay ="";
+                             
+                       
+            try
+            {
+                Console.WriteLine("Enter Current Day of Week");
+                currentDay = Console.ReadLine();
+                DaysOfTheWeek daysOfTheWeek1 = (DaysOfTheWeek)System.Enum.Parse(typeof(DaysOfTheWeek), currentDay);
+                Console.WriteLine(daysOfTheWeek1);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+                
+
+                
+             
+             Console.ReadLine();
+                
+              Console.WriteLine("Please enter an actual day of the week. Please!");
+                
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ step195-EnumAnnndddd/days.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step195_EnumAnnndddd
+{
+    class days
+    {
+    }
+    public enum DaysOfTheWeek
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wedensday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
