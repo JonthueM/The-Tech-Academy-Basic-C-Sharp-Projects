a124f573e68fe117d41f53a50a6b7b7c2c9099b4
JonthueM <connect@jonthuemichel.com>
2020-07-11 11:21:10 -0400

    Assignment 210 - IO - Log Response

---
+++ step210-IO-LogResponse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step210_IO_LogResponse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Path
+            string path = @"C:\Users\conne\Documents\log-210.txt";
+            Console.WriteLine("Hello There! Please insert a random number below");
+            string randomNumber = Console.ReadLine();
+            File.WriteAllText(path, randomNumber);
+            Console.WriteLine("Retreiving from Written File");
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ step210-IO-LogResponse/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("step210-IO-LogResponse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("step210-IO-LogResponse")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2466029c-1726-453c-8b50-76a2ebe688d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
