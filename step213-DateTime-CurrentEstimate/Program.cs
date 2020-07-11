ec65f7a17b4bf91a87869ea5b912740704888717
JonthueM <connect@jonthuemichel.com>
2020-07-11 12:31:39 -0400

    step213-DateTime

---
+++ step213-DateTime-CurrentEstimate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace step213_DateTime_CurrentEstimate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Current Time&Date: {DateTime.Now}");
+            Console.WriteLine("Type a Number Below to see what it will be hours from now!");
+            int xHours = Convert.ToInt32(Console.ReadLine());
+            DateTime current = DateTime.Now;
+            DateTime future = current.AddHours(xHours);
+            Console.WriteLine(future);
+
+            Console.ReadLine();
+
+        }
+    }
+}
