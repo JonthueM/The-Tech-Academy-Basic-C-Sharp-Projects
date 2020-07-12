bb1b3bf98c20653dffac923a3d38c4eef87e2060
JonthueM <connect@jonthuemichel.com>
2020-07-11 20:05:25 -0400

    2020 step225 CatchIt

--- 2020-step221-VarVarConstruct/Program.cs
+++ 2020-step221-VarVarConstruct/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,17 @@ namespace _2020_step221_VarVarConstruct
     {
         static void Main(string[] args)
         {
+            //Var
+            var y = 10;
+
+            //Consruct Variable
+            const int x = 10;
+
+            Console.WriteLine($"{y} {x}");
+
+            
+
+            
         }
     }
 }
