fe2af9b72b2c40035b945ae93f91c749aa880a34
JonthueM <connect@jonthuemichel.com>
2020-07-27 23:19:59 -0400

    Fix & Rename

--- 2020-step161-3Deep/Operations.cs
+++ 2020-step161-3Deep/Operations.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace step131_3Deep
+namespace step161_3Deep
 {
     class Operations
     {
--- 2020-step234-CatchThis/Program.cs
+++ 2020-step234-CatchThis/Program.cs
@@ -10,45 +10,27 @@ namespace _2020_step234_CatchThis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How Old Are You This Year?");
-            int age;
-            age = Convert.ToInt32(Console.ReadLine());
-
-            if (age >0)
-            {
-                int birthYear = age - 2020;
-                Console.WriteLine(birthYear);
-            }
-        
-
             try
             {
+                Console.WriteLine("How Old Are You This Year?");
+                int age;
+                age = Convert.ToInt32(Console.ReadLine());
+
                 if (age == 0)
                 {
                     throw new ArgumentException("Please No Zero's");
-
                 }
-                
-            }
-
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine("Please No Zero's");
-            }
-            try
-            {
                 if (age < 0)
                 {
                     throw new ArgumentOutOfRangeException("Why So Negative");
                 }
-                
+
+                int birthYear = age - 2020;
+                Console.WriteLine(birthYear);
             }
-            catch (ArgumentOutOfRangeException i)
-            {
-                Console.WriteLine("Why So Negative");
-            }
-            
-            
+
+
+
 
             Console.ReadLine();
         }
